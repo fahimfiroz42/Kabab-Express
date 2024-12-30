d557347d301615511bf66d3dd5193f76edbed184
fahimfiroz42 <fahimfiroz.bd42@gmail.com>
2024-12-31 00:00:32 +0600

    navbar design implement

--- kabab-express-client/src/Components/Navbar.jsx
+++ kabab-express-client/src/Components/Navbar.jsx
@@ -23,39 +23,32 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+          <li><a>HOME</a></li>
+      <li><a>CONTACT US</a></li>
+      <li><a>DASHBOARD</a></li>
+      <li><a>OUR MENU</a></li>
+      <li><a>OUR SHOP</a></li>
+     
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl"> <span className="font-bold text-">KABAB</span> EXPRESS</a>
   </div>
-  <div className="navbar-center hidden lg:flex">
+  <div className="flex-end flex-shrink-0 hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      <li><a>HOME</a></li>
+      <li><a>CONTACT US</a></li>
+      <li><a>DASHBOARD</a></li>
+      <li><a>OUR MENU</a></li>
+      <li><a>OUR SHOP</a></li>
+     
+     
     </ul>
   </div>
-  <div className="navbar-end">
+  <div className="navbar-end w-auto">
     <a className="btn">Button</a>
   </div>
 </div>
-            
+
             
         </div>
     );
