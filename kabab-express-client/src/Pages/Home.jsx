6a07aa29e5c20d6ddd7679ce6d3d7ece007ae3b7
fahimfiroz42 <fahimfiroz.bd42@gmail.com>
2025-01-02 23:28:56 +0600

    dynamic page title added

--- kabab-express-client/src/Pages/Home.jsx
+++ kabab-express-client/src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../Components/Banner";
 import BistroBanner from "../Components/BistroBanner";
 import Category from "../Components/Category";
@@ -8,6 +9,9 @@ import Testimonials from "../Components/Testimonials";
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Kabab Express | Home</title>
+            </Helmet>
             <Banner/>
             <Category/>
             <BistroBanner/>
